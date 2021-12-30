ee7c18d9b13be3cf9e21213bf8e206407ef8fb80
Error::3080 <87394718+KurRuppted@users.noreply.github.com>
2021-12-30 11:50:41 -0800

    Add files via upload

---
+++ Challenge 4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random numberGen01 = new Random();
+            Random numberGen02 = new Random();
+
+            int roll01 = 2;
+            int roll02 = 1;
+            int attempts = 0; 
+
+            Console.WriteLine("Press enter to roll the two dices!");
+
+            while(roll01  != roll02)
+            {   
+                Console.ReadKey();
+
+                roll01 = numberGen01.Next(1,7); 
+                roll02 = numberGen02.Next(1,7); 
+                Console.WriteLine("Die 1: " + roll01);
+                Console.WriteLine("Die 2: " + roll02);
+                attempts ++;
+            }
+
+            Console.WriteLine("It took you " + attempts + " attempts to match both dies!");
+            
+
+
+            Console.ReadKey();
+        }
+    }
+}
